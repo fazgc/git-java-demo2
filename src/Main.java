99a8b1c3c9332e9ab0bcd1b5387c31399a42902c
faiza <faizaganci@gmail.com>
2023-03-12 16:29:29 +0000

    commit second java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello IT world!");
         System.out.println("hello commit");
+        System.out.println("commit -3");
     }
 }
